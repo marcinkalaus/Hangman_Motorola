72895248c0cd19ec7b7de450c993c402de4f03b1
marcinkalaus <marcinkalaus@gmail.com>
2020-11-30 16:40:06 +0100

    v0.4
    - messages to user has been implemented
    - code refactored to avoid exceptions

--- Hangman_Motorola/Program.cs
+++ Hangman_Motorola/Program.cs
@@ -32,6 +32,17 @@ namespace Hangman_Motorola
             int wordNumber = random.Next(0, locations.Length);
             return wordNumber; 
         }
+        string readLetter()
+        {
+            Console.WriteLine("Choose your letter");
+            return Console.ReadLine().ToUpper();
+        }
+
+        string readWord()
+        {
+            Console.WriteLine("Write your word");
+            return Console.ReadLine().ToUpper();
+        }
 
         void repeatGame()
         {
@@ -43,9 +54,13 @@ namespace Hangman_Motorola
             if (option == 1)
             {
                 play();
-            } else
+            } else if (option == 2)
             {
                 Environment.Exit(0);
+            } else
+            {
+                Console.WriteLine("Choose correct option!");
+                repeatGame();
             }
         }
 
@@ -57,7 +72,7 @@ namespace Hangman_Motorola
             countriesAndCapitalsCreator(locations);
 
             int wordNumber = wordNumberGenerator(locations);
-            string wordToGuess = capitals[166].ToUpper();
+            string wordToGuess = capitals[wordNumber].ToUpper();
 
             StringBuilder dashWord = new StringBuilder(wordToGuess.Length);
             for (int i = 0; i < wordToGuess.Length; i++)
@@ -69,7 +84,7 @@ namespace Hangman_Motorola
                 }
 
             int lives = 5;
-            Console.WriteLine(wordToGuess);
+           // Console.WriteLine(wordToGuess);
             while (lives > 0)
             {
                 Console.WriteLine("Lives: {0}", lives);
@@ -77,63 +92,81 @@ namespace Hangman_Motorola
                 {
                     Console.WriteLine("The Capital of {0}", countries[wordNumber]);
                 }
-                Console.WriteLine("Choose your letter");
+
                 Console.WriteLine(dashWord);
-                string givenLetter = Console.ReadLine().ToUpper();
-                if (wordToGuess.Contains(givenLetter))
+
+                Console.WriteLine("Would you like to guess a letter or the whole word?");
+                Console.WriteLine("1 - letter, 2 - word");
+
+                
+                int guessOption = Int32.Parse(Console.ReadLine());
+
+
+                if (guessOption == 1)
                 {
+                    string givenLetter = readLetter();
 
-                    int letterPosition = wordToGuess.IndexOf(givenLetter);
-                    dashWord.Replace(dashWord[letterPosition], givenLetter[0], letterPosition, 1);
-
-                    while(wordToGuess.IndexOf(givenLetter, letterPosition + 1) != -1)
+                    if (wordToGuess.Contains(givenLetter))
                     {
-                        letterPosition = wordToGuess.IndexOf(givenLetter, letterPosition + 1);
-                        dashWord.Replace(dashWord[letterPosition], givenLetter[0], letterPosition, 1);
-                    }
 
-                    if (dashWord.Equals(wordToGuess))
+                        int letterPosition = wordToGuess.IndexOf(givenLetter);
+                        dashWord.Replace(dashWord[letterPosition], givenLetter[0], letterPosition, 1);
+
+                        while (wordToGuess.IndexOf(givenLetter, letterPosition + 1) != -1)
+                        {
+                            letterPosition = wordToGuess.IndexOf(givenLetter, letterPosition + 1);
+                            dashWord.Replace(dashWord[letterPosition], givenLetter[0], letterPosition, 1);
+                        }
+
+                        if (dashWord.Equals(wordToGuess))
+                        {
+                            Console.WriteLine("---------------------------------------------------");
+                            Console.WriteLine("You won!");
+                            repeatGame();
+                        }
+
+                    }
+                    else
+                    {
+                        lives--;
+
+                        if (!lettersNotInWord.Contains(givenLetter))
+                        {
+                            lettersNotInWord.Add(givenLetter);
+                        }
+                        Console.WriteLine("---------------------------------------------------");
+                        Console.WriteLine(string.Format("Missed letters: {0}", string.Join(", ", lettersNotInWord)));
+                    }
+                } else if (guessOption == 2)
+                {
+                    string givenWord = readWord();
+
+                    if (givenWord.Equals(wordToGuess))
                     {
                         Console.WriteLine("You won!");
+                        Console.WriteLine("---------------------------------------------------");
                         break;
+                    } else
+                    {
+                        Console.WriteLine("---------------------------------------------------");
+                        lives -=2;
                     }
-
                 } else
                 {
-                    lives--;
-
-                    if (!lettersNotInWord.Contains(givenLetter))
-                    {
-                        lettersNotInWord.Add(givenLetter);
-                    }
-                    Console.WriteLine(string.Format("Missed letters: {0}", string.Join(", ", lettersNotInWord)));
-
-                    
-
+                    Console.WriteLine("---------------------------------------------------");
+                    Console.WriteLine("Type correct option!");
                 }
-
             }
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("You lose! :(");
             repeatGame();
-
         }
             
         
         static void Main(string[] args)
         {
-
             Program game = new Program();
             game.play();
-            
-            //Console.WriteLine(program.wordNumberGenerator(locations));
-
-         /*   for (int i=0; i<10; i++)
-            {
-                Console.WriteLine(countries[i]);
-                Console.WriteLine(capitals[i]);
-            }*/
-
-        }
-
-       
+        }  
     }
 }
